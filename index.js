d701f480b1e234bec6de7b075c1ce67dc89c83e7
Bohdan Petryshyn <b.y.petryshyn@gmail.com>
2025-02-22 22:07:13 +0800

    feat: initial implementation

---
+++ index.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const widget = document.querySelector("elevenlabs-convai");
+
+  if (widget) {
+    console.log("PromptUser tool initialized");
+
+    widget.addEventListener("elevenlabs-convai:call", (event) => {
+      event.detail.config.clientTools = {
+        // TODO: use original tool name
+        promptUser: ({id, formHtml, formCss}) => {
+          return new Promise((resolve) => {
+            renderForm(id, formHtml, formCss, (values) => {
+              console.log("onSubmit", JSON.stringify(values));
+              resolve(JSON.stringify(values));
+            });
+          });
+        },
+      };
+    });
+  }
+});
+
+function renderForm(id, formHtml, formCss, onSubmit) {
+  console.log("renderForm", id);
+  console.log("formHtml", formHtml);
+  console.log("formCss", formCss);
+
+  const formId = `prompt-user-form-${id}`;
+  let form = document.getElementById(formId);
+
+  if (!form) {
+    form = document.createElement("form");
+    form.innerHTML = `
+      <style>${formCss}</style>
+      ${formHtml}
+      <button type="submit">OK</button>
+
+    `
+  
+    document.body.appendChild(form);
+
+    const firstInput = form.querySelector("input");
+    if (firstInput) firstInput.focus();
+  }
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(form));
+
+    form.remove();
+    onSubmit(formData);
+  };
+}
